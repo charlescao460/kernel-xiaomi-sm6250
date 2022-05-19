39295c907064f193e5b52a371997a413b2d31a18
charlescao460 <charlescao@csr460.com>
2022-05-19 16:02:00 -0400

    Working build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,12 @@
 COMMON_PATH="$(cd "$(dirname "$0")/../" >/dev/null 2>&1 && pwd)"
 KERNEL_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
 
-export PATH=$COMMON_PATH/proton-clang/bin/:$COMMON_PATH/aarch64-linux-android/bin/:${PATH}
+export PATH=$COMMON_PATH/linux-x86/clang-r353983c/bin/:$COMMON_PATH/aarch64-linux-android/bin/:${PATH}
 export ARCH=arm64
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=aarch64-linux-android-
-export LD_LIBRARY_PATH=$COMMON_PATH/proton-clang/lib
+export LD_LIBRARY_PATH=$COMMON_PATH/linux-x86/clang-r353983c/lib64
 
 cd $KERNEL_PATH
-make -j$(nproc --all) CC=clang $@
+make -j$(nproc --all) CC=clang custj-stock_defconfig
+make -j$(nproc --all) CC=clang
